6debbf71df04d6208c297617a15958072dbea92b
Rob Wiegman <robwiegman@Robs-MBP.fritz.box>
2016-01-23 14:34:58 +0100

    - Fixed bug in determining kosten in period

--- src/main/java/nl/wiegman/homecontrol/services/repository/KostenRepository.java
+++ src/main/java/nl/wiegman/homecontrol/services/repository/KostenRepository.java
@@ -17,7 +17,7 @@ public interface KostenRepository extends JpaRepository<Kosten, Long> {
     String CACHE_NAME_KOSTEN_IN_PERIOD = "kostenInPeriod";
 
     @Cacheable(cacheNames = CACHE_NAME_KOSTEN_IN_PERIOD)
-    @Query(value = "SELECT k FROM Kosten k WHERE (:van BETWEEN k.van AND k.totEnMet) OR (:totEnMet BETWEEN k.van AND k.totEnMet) ORDER BY k.van")
+    @Query(value = "SELECT k FROM Kosten k WHERE (:van BETWEEN k.van AND k.totEnMet) OR (:totEnMet BETWEEN k.van AND k.totEnMet) OR (k.van >= :van AND k.totEnMet <= :totEnMet) ORDER BY k.van")
     List<Kosten> getKostenInPeriod(@Param("van") long van, @Param("totEnMet") long totEnMet);
 
 }
