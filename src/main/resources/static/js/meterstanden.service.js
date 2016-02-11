e88e9cda6cec5a4be07359e1ad167652defb6560
Rob Wiegman <robwiegman@Robs-MBP.fritz.box>
2016-02-11 20:28:17 +0100

    Added logging

--- src/main/resources/static/js/meterstanden.service.js
+++ src/main/resources/static/js/meterstanden.service.js
@@ -5,14 +5,18 @@
         .module('app')
         .service('MeterstandenService', MeterstandenService);
 
-    MeterstandenService.$inject = ['$http'];
+    MeterstandenService.$inject = ['$http', '$log'];
 
-    function MeterstandenService($http) {
+    function MeterstandenService($http, $log) {
 
         this.getMeterstandenPerDagInPeriod = function(van, totEnMet) {
 
+            var url = 'rest/meterstanden/per-dag/' + van + '/' + totEnMet;
+
+            $log.info('Getting data for meterstanden from URL: ' + url);
+
             return $http({
-                method: 'GET', url: 'rest/meterstanden/per-dag/' + van + '/' + totEnMet
+                method: 'GET', url: url
             });
         };
 
