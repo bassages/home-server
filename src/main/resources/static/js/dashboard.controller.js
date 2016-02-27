f91cf12fd26073245d464ba44e24492988783d02
Rob Wiegman <robwiegman@Robs-MBP.fritz.box>
2016-02-27 18:05:25 +0100

    Fixed time related bug

--- src/main/resources/static/js/dashboard.controller.js
+++ src/main/resources/static/js/dashboard.controller.js
@@ -37,30 +37,25 @@
         });
 
         function update(data) {
-            var now = (new Date()).getTime();
-            if (data.datumtijd > now - 600000) {
-                $scope.t1 = data.stroomTarief1;
-                $scope.t2 = data.stroomTarief2;
-                $scope.meterstandGas = data.gas;
-                $scope.huidigOpgenomenVermogen = data.stroomOpgenomenVermogenInWatt;
+            $scope.t1 = data.stroomTarief1;
+            $scope.t2 = data.stroomTarief2;
+            $scope.meterstandGas = data.gas;
+            $scope.huidigOpgenomenVermogen = data.stroomOpgenomenVermogenInWatt;
 
-                var step = 150;
-                $scope.stroomLed0 = data.stroomOpgenomenVermogenInWatt > 0;
-                $scope.stroomLed1 = data.stroomOpgenomenVermogenInWatt >= (step);
-                $scope.stroomLed2 = data.stroomOpgenomenVermogenInWatt >= (2 * step);
-                $scope.stroomLed3 = data.stroomOpgenomenVermogenInWatt >= (3 * step);
-                $scope.stroomLed4 = data.stroomOpgenomenVermogenInWatt >= (4 * step);
-                $scope.stroomLed5 = data.stroomOpgenomenVermogenInWatt >= (5 * step);
-                $scope.stroomLed6 = data.stroomOpgenomenVermogenInWatt >= (6 * step);
-                $scope.stroomLed7 = data.stroomOpgenomenVermogenInWatt >= (7 * step);
-                $scope.stroomLed8 = data.stroomOpgenomenVermogenInWatt >= (8 * step);
-                $scope.stroomLed9 = data.stroomOpgenomenVermogenInWatt >= (9 * step);
+            var step = 150;
+            $scope.stroomLed0 = data.stroomOpgenomenVermogenInWatt > 0;
+            $scope.stroomLed1 = data.stroomOpgenomenVermogenInWatt >= (step);
+            $scope.stroomLed2 = data.stroomOpgenomenVermogenInWatt >= (2 * step);
+            $scope.stroomLed3 = data.stroomOpgenomenVermogenInWatt >= (3 * step);
+            $scope.stroomLed4 = data.stroomOpgenomenVermogenInWatt >= (4 * step);
+            $scope.stroomLed5 = data.stroomOpgenomenVermogenInWatt >= (5 * step);
+            $scope.stroomLed6 = data.stroomOpgenomenVermogenInWatt >= (6 * step);
+            $scope.stroomLed7 = data.stroomOpgenomenVermogenInWatt >= (7 * step);
+            $scope.stroomLed8 = data.stroomOpgenomenVermogenInWatt >= (8 * step);
+            $scope.stroomLed9 = data.stroomOpgenomenVermogenInWatt >= (9 * step);
 
-                if ($scope.oudsteVanVandaag != null) {
-                    $scope.gasVerbruikVandaag = data.gas - $scope.oudsteVanVandaag.gas;
-                }
-            } else {
-                $log.warn("Discarding data, because it's too old: " + JSON.stringify(data));
+            if ($scope.oudsteVanVandaag != null) {
+                $scope.gasVerbruikVandaag = data.gas - $scope.oudsteVanVandaag.gas;
             }
         }
     }
