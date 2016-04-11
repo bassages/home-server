a09d41ed2616c89785c487c81c88a15bcb39108c
Rob Wiegman <robwiegman@Robs-MacBook-Pro.local>
2016-04-11 20:38:30 +0200

    Added Odometer (first draft/POC)

--- src/main/resources/static/js/odometer.directive.js
+++ src/main/resources/static/js/odometer.directive.js
@@ -10,12 +10,17 @@
             restrict: 'A',
             link: function(scope, element, attrs) {
 
-                new Odometer({
-                        el: element[0],
-                        value: scope[attrs.odometer],
-                        duration: 1000
-                    }
-                );
+                var options = {
+                    el: element[0],
+                    value: scope[attrs.odometer],
+                    duration: 1000
+                };
+
+                if (attrs.format) {
+                    options.format = attrs.format;
+                }
+
+                new Odometer(options);
 
                 //Watch for changes and update the element value (causing odometer to redraw)
                 scope.$watch(attrs.odometer, function(val) {
