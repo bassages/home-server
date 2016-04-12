1ec1f8c83f7c37e8963c881eb6911c63804d487d
Rob Wiegman <robwiegman@Robs-MBP.fritz.box>
2016-04-12 19:36:36 +0200

    Odometer for all dashboard values

--- src/main/resources/static/js/odometer.directive.js
+++ src/main/resources/static/js/odometer.directive.js
@@ -5,8 +5,12 @@
         .module('app')
         .directive('odometer', odometer);
 
+    // TODO: https://github.com/HubSpot/odometer/issues/106
+    // An alternative could be: http://scrollerjs.pixelstech.net/
+
     function odometer () {
         return {
+
             restrict: 'A',
             link: function(scope, element, attrs) {
 
@@ -16,15 +20,39 @@
                     duration: 1000
                 };
 
-                if (attrs.format) {
-                    options.format = attrs.format;
+                var nrOfDecimals = 0;
+                if (attrs.nrofdecimals) {
+                    nrOfDecimals = attrs.nrofdecimals;
                 }
 
-                new Odometer(options);
+                var odoformat = "(.ddddddddd)";
+                if (nrOfDecimals > 0) {
+                    odoformat = odoformat + ',';
+                    for (var i = 0; i < nrOfDecimals; i++) {
+                        odoformat = odoformat + 'd';
+                    }
+                    odoformat = odoformat + 'd';
+                }
+
+                options.format = odoformat;
+
+                var od = new Odometer(options);
 
                 //Watch for changes and update the element value (causing odometer to redraw)
                 scope.$watch(attrs.odometer, function(val) {
-                    element.text(val);
+                    if (val != null) {
+
+                        if (nrOfDecimals == 1) {
+                            val = val + 0.01;
+                        } else if (nrOfDecimals == 2) {
+                            val = val + 0.001;
+                        } else if (nrOfDecimals == 3) {
+                            val = val + 0.0001;
+                        } else if (nrOfDecimals == 4) {
+                            val = val + 0.00001;
+                        }
+                        od.update(val.toString().replace('.', ',') + '1');
+                    }
                 });
 
             }
