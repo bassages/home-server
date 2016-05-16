044c5af6b212b5e7959a6224d455fc6296d4c092
Rob Wiegman <robwiegman@Robs-MBP.fritz.box>
2016-05-16 20:04:11 +0200

    Added some logging to try to find out why modal is sometimes not closed

--- src/main/resources/static/app/loading-indicator/loading-indicator.service.js
+++ src/main/resources/static/app/loading-indicator/loading-indicator.service.js
@@ -5,9 +5,9 @@
         .module('app')
         .service('LoadingIndicatorService', LoadingIndicatorService);
 
-    LoadingIndicatorService.$inject = ['$uibModal', '$log'];
+    LoadingIndicatorService.$inject = ['$uibModal', '$log', '$q'];
 
-    function LoadingIndicatorService($uibModal, $log) {
+    function LoadingIndicatorService($uibModal, $log, $q) {
         var loadingModalInstance = null;
 
         this.startLoading = function() {
@@ -17,11 +17,39 @@
                 size: 'sm',
                 backdrop: 'static'
             });
+
+            $q.all(loadingModalInstance.opened).then(
+                function successCallback(response) {
+                    $log.info("Opened")
+                },
+                function errorCallback(response) {
+                    $log.info("Failed open")
+                });
+
+            $q.all(loadingModalInstance.rendered).then(
+                function successCallback(response) {
+                    $log.info("Rendered")
+                },
+                function errorCallback(response) {
+                    $log.info("Failed render")
+                });
+
+            $q.all(loadingModalInstance.closed).then(
+                function successCallback(response) {
+                    $log.info("Closed")
+                },
+                function errorCallback(response) {
+                    $log.info("Failed close")
+                });
         };
 
         this.stopLoading = function() {
-            loadingModalInstance.close();
+            $log.info("Closing modal");
+
+            loadingModalInstance.opened.then(loadingModalInstance.close());
             loadingModalInstance = null;
+
+            $log.info("Closed modal");
         };
     }
 })();
