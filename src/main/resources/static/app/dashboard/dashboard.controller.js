9de160a25269a6cb0eb7b87b480b4cee93e3aa10
Rob Wiegman <robwiegman@Robs-MBP.fritz.box>
2016-05-17 21:25:59 +0200

    Added (implemented) leds to temperatuur and (unimplemented) leds to humidity

--- src/main/resources/static/app/dashboard/dashboard.controller.js
+++ src/main/resources/static/app/dashboard/dashboard.controller.js
@@ -9,7 +9,7 @@
 
     function turnOffAllStroomLeds($scope) {
         for (var i = 0; i < 10; i++) {
-            $scope['stroomLed' + i] = false;
+            $scope['opgenomenVermogenLed' + i] = false;
         }
     }
 
@@ -94,7 +94,32 @@
         function updateKlimaat(data) {
             if (data != null) {
                 $scope.huidigKlimaat = data;
+                setTemperatuurLeds(data.temperatuur);
             }
+        }
+
+        function setOpgenomenVermogenLeds(stroomOpgenomenVermogenInWatt) {
+            var step = 150;
+
+            $scope.opgenomenVermogenLed0 = stroomOpgenomenVermogenInWatt > 0;
+
+            for (var i = 1; i < 9; i++) {
+                $scope['opgenomenVermogenLed' + i] = stroomOpgenomenVermogenInWatt >= (i * step);
+
+            }
+        }
+
+        function setTemperatuurLeds(temperatuur) {
+            $scope.temperatuurLed9 = temperatuur >= 25; //&& temperatuur < 26;
+            $scope.temperatuurLed8 = temperatuur >= 24; //&& temperatuur < 25;
+            $scope.temperatuurLed7 = temperatuur >= 23; //&& temperatuur < 24;
+            $scope.temperatuurLed6 = temperatuur >= 22; //&& temperatuur < 23;
+            $scope.temperatuurLed5 = temperatuur >= 21; //&& temperatuur < 22;
+            $scope.temperatuurLed4 = temperatuur >= 20; //&& temperatuur < 21;
+            $scope.temperatuurLed3 = temperatuur >= 19; //&& temperatuur < 20;
+            $scope.temperatuurLed2 = temperatuur >= 18; //&& temperatuur < 19;
+            $scope.temperatuurLed1 = temperatuur >= 17; //&& temperatuur < 18;
+            $scope.temperatuurLed0 = true;
         }
 
         function updateMeterstand(data) {
@@ -109,17 +134,7 @@
                 $scope.meterstandGas = data.gas;
                 $scope.huidigOpgenomenVermogen = data.stroomOpgenomenVermogenInWatt;
 
-                var step = 150;
-                $scope.stroomLed0 = data.stroomOpgenomenVermogenInWatt > 0;
-                $scope.stroomLed1 = data.stroomOpgenomenVermogenInWatt >= (step);
-                $scope.stroomLed2 = data.stroomOpgenomenVermogenInWatt >= (2 * step);
-                $scope.stroomLed3 = data.stroomOpgenomenVermogenInWatt >= (3 * step);
-                $scope.stroomLed4 = data.stroomOpgenomenVermogenInWatt >= (4 * step);
-                $scope.stroomLed5 = data.stroomOpgenomenVermogenInWatt >= (5 * step);
-                $scope.stroomLed6 = data.stroomOpgenomenVermogenInWatt >= (6 * step);
-                $scope.stroomLed7 = data.stroomOpgenomenVermogenInWatt >= (7 * step);
-                $scope.stroomLed8 = data.stroomOpgenomenVermogenInWatt >= (8 * step);
-                $scope.stroomLed9 = data.stroomOpgenomenVermogenInWatt >= (9 * step);
+                setOpgenomenVermogenLeds(data.stroomOpgenomenVermogenInWatt);
 
                 if ($scope.oudsteVanVandaag != null) {
                     $scope.gasVerbruikVandaag = data.gas - $scope.oudsteVanVandaag.gas;
