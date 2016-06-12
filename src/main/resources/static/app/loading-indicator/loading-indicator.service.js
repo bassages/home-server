b4ee08b37f729e18f141f570484b5519269ff644
Rob Wiegman <robwiegman@Robs-MBP.fritz.box>
2016-06-12 16:21:48 +0200

    Another workaround for dialog sometimes not closing

--- src/main/resources/static/app/loading-indicator/loading-indicator.service.js
+++ src/main/resources/static/app/loading-indicator/loading-indicator.service.js
@@ -10,7 +10,9 @@
     function LoadingIndicatorService($uibModal, $log, $q) {
         var loadingModalInstance = null;
 
-        this.startLoading = function(functionToCallWhenDialogIsOpened) {
+        removeModalElements();
+
+        this.startLoading = function() {
             loadingModalInstance = $uibModal.open({
                 animation: false,
                 templateUrl: 'app/loading-indicator/loading-indicator-dialog.html',
@@ -21,23 +23,33 @@
             $q.all(loadingModalInstance.opened, loadingModalInstance.rendered).then(
                 function successCallback(response) {
                     $log.info("Opened and rendered");
-
-                    if (functionToCallWhenDialogIsOpened && typeof functionToCallWhenDialogIsOpened === 'function') {
-                        functionToCallWhenDialogIsOpened();
-                    }
                 },
                 function errorCallback(response) {
                     $log.info("Failed open and/or render");
                 });
         };
 
+        function removeModalElements() {
+
+            if (!loadingModalInstance) {
+                // Workaround for issue where somtimes the dialog is not closed
+                var modalBackdrop = angular.element(document).find('.modal-backdrop');
+                if (modalBackdrop.length > 0) {
+                    modalBackdrop.remove();
+                    $log.info("Removed backdrop");
+                }
+                var modal = angular.element(document).find('.modal');
+                if (modal.length > 0) {
+                    modal.remove();
+                    $log.info("Removed modal");
+                }
+                setTimeout(removeModalElements, 1000);
+            }
+        }
+
         this.stopLoading = function() {
             loadingModalInstance.close();
             loadingModalInstance = null;
-
-            // Workaround for issue where somtimes the dialog is nog closed
-            angular.element(document).find('.modal-backdrop').remove();
-            angular.element(document).find('.modal').remove();
         };
     }
 })();
