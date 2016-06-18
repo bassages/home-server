e1be3bf0eb9e3d0483edd10bb6267b251dc67bb3
Rob Wiegman <robwiegman@Robs-MBP.fritz.box>
2016-06-18 12:26:34 +0200

    Another workaround for dialog sometimes not closing

--- src/main/resources/static/app/klimaat-historie/klimaat-historie.controller.js
+++ src/main/resources/static/app/klimaat-historie/klimaat-historie.controller.js
@@ -321,7 +321,7 @@
             loadData([]);
             if ($scope.selection.length > 0) {
 
-                var loading = LoadingIndicatorService.startLoading();
+                $scope.loading = LoadingIndicatorService.startLoading();
 
                 var requests = [];
 
@@ -334,11 +334,11 @@
                 $q.all(requests).then(
                     function successCallback(response) {
                         loadData(transformServerdata(response));
-                        loading.close();
+                        $scope.loading.close();
                     },
                     function errorCallback(response) {
                         $log.error(JSON.stringify(response));
-                        loading.close();
+                        $scope.loading.close();
                         ErrorMessageService.showMessage("Er is een fout opgetreden bij het ophalen van de gegevens");
                     }
                 );
