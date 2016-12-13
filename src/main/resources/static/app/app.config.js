702a17b30cfd3ba4bda9140a7679b061a8aae0ed
Rob Wiegman <rob.wiegman@luminis.eu>
2016-12-13 20:37:46 +0100

    more minification, updated versions of wro4j and less4j

--- src/main/resources/static/app/app.config.js
+++ src/main/resources/static/app/app.config.js
@@ -1,6 +1,13 @@
 (function() {
     'use strict';
 
+    angular.module('app', [
+        'ngRoute',
+        'ngResource',
+        'ngAnimate',
+        'ui.bootstrap'
+    ]);
+
     angular
         .module('app')
         .config(Config);
