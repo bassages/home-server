ef61d2ef71c12ebe5c4fcab3785432980ecac88f
Rob Wiegman <rob.wiegman@luminis.eu>
2016-12-13 21:41:54 +0100

    Undo: Added cache headers for static resources

--- src/main/java/nl/wiegman/home/config/SecurityConfig.java
+++ src/main/java/nl/wiegman/home/config/SecurityConfig.java
@@ -1,21 +1,21 @@
-package nl.wiegman.home.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/static/**");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // Prevent the HTTP response header of "Pragma: no-cache".
-        http.headers().cacheControl().disable();
-    }
-}
+//package nl.wiegman.home.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring().antMatchers("/static/**");
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        // Prevent the HTTP response header of "Pragma: no-cache".
+//        http.headers().cacheControl().disable();
+//    }
+//}
