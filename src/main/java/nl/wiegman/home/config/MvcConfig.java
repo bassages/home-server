c72006578b6da0497a2cf2d26343ed4fc5d5e81e
Rob Wiegman <rob.wiegman@luminis.eu>
2016-12-16 18:14:20 +0100

    Prevent caching of index.html as it causes trouble with basic auth

--- src/main/java/nl/wiegman/home/config/MvcConfig.java
+++ src/main/java/nl/wiegman/home/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package nl.wiegman.home.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -11,6 +12,9 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/index.html")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.noStore());
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/static/")
                 .setCachePeriod((int) TimeUnit.DAYS.toSeconds(365l));
