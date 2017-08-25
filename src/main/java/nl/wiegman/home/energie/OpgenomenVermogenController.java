a4ec4bfd2c0c7451508483f7208cefc7ceeaa733
Rob Wiegman <rob.wiegman@luminis.eu>
2017-08-25 18:22:22 +0200

    Removed cleanup endpoints from OpgenomenVermogenController

--- src/main/java/nl/wiegman/home/energie/OpgenomenVermogenController.java
+++ src/main/java/nl/wiegman/home/energie/OpgenomenVermogenController.java
@@ -1,10 +1,8 @@
 package nl.wiegman.home.energie;
 
-import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
 
-import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,28 +35,4 @@ public class OpgenomenVermogenController {
             return opgenomenVermogenService.getHistory(new Date(from), new Date(to), subPeriodLength);
         }
     }
-
-    @GetMapping(path = "cleanup/{day}")
-    public void cleanup(@PathVariable("day") long day) {
-        opgenomenVermogenService.cleanup(new Date(day));
-    }
-
-
-    @GetMapping(path = "cleanup")
-    public void cleanup() {
-        try {
-            Date twoDaysAgo = DateUtils.addDays(new Date(), -2);
-
-            Date date = DateUtils.parseDate("26-02-2017", "dd-MM-yyyy");
-
-            while (date.before(twoDaysAgo)) {
-                opgenomenVermogenService.cleanup(date);
-                date = DateUtils.addDays(date, 1);
-            }
-
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-    }
-
 }
