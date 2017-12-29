5272a879555924a2a89cc6c0663bc851dbf8c4e4
Rob Wiegman <rob.wiegman@luminis.eu>
2017-12-29 11:11:40 +0100

    Replace using Date and millis by LocalDate, LocalDateTime and Period classes

--- src/test/java/nl/wiegman/home/klimaat/KlimaatSensorValueTrendServiceTest.java
+++ src/test/java/nl/wiegman/home/klimaat/KlimaatSensorValueTrendServiceTest.java
@@ -1,16 +1,16 @@
 package nl.wiegman.home.klimaat;
 
+import static java.time.Month.JULY;
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
-import java.text.ParseException;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
-import org.apache.commons.lang3.time.DateUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,7 +18,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import nl.wiegman.home.DateTimeUtil;
 import nl.wiegman.home.Trend;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -38,61 +37,57 @@ public class KlimaatSensorValueTrendServiceTest {
     @Test
     public void whenTemperatureRisesThenTrendIsUp() throws Exception {
         List<Klimaat> klimaats = asList(
-                getKlimaat(toDate("01-01-2016 14:00:00"), new BigDecimal("19.00")),
-                getKlimaat(toDate("01-01-2016 14:04:00"), new BigDecimal("20.20")),
-                getKlimaat(toDate("01-01-2016 14:08:00"), new BigDecimal("21.70"))
+                createKlimaat(LocalDate.of(2016, JULY, 1).atTime(14, 0, 0), new BigDecimal("19.00")),
+                createKlimaat(LocalDate.of(2016, JULY, 1).atTime(14, 4, 0), new BigDecimal("20.20")),
+                createKlimaat(LocalDate.of(2016, JULY, 1).atTime(14, 8, 0), new BigDecimal("21.70"))
         );
 
         assertThat(klimaatSensorValueTrendService.determineValueTrend(klimaats, Klimaat::getTemperatuur)).isEqualTo(Trend.UP);
     }
 
     @Test
-    public void whenTemperatureDropsThenTrendIsDown() throws Exception {
+    public void whenTemperatureDropsThenTrendIsDown() {
         List<Klimaat> klimaats = asList(
-            getKlimaat(toDate("01-01-2016 14:00:00"), new BigDecimal("21.70")),
-            getKlimaat(toDate("01-01-2016 14:04:00"), new BigDecimal("20.20")),
-            getKlimaat(toDate("01-01-2016 14:08:00"), new BigDecimal("19.00"))
+            createKlimaat(LocalDate.of(2016, JULY, 1).atTime(14, 0, 0), new BigDecimal("21.70")),
+            createKlimaat(LocalDate.of(2016, JULY, 1).atTime(14, 4, 0), new BigDecimal("20.20")),
+            createKlimaat(LocalDate.of(2016, JULY, 1).atTime(14, 8, 0), new BigDecimal("19.00"))
         );
 
         assertThat(klimaatSensorValueTrendService.determineValueTrend(klimaats, Klimaat::getTemperatuur)).isEqualTo(Trend.DOWN);
     }
 
     @Test
-    public void whenTemperatureIsSameThenTrendIsStable() throws Exception {
+    public void whenTemperatureIsSameThenTrendIsStable() {
         List<Klimaat> klimaats = asList(
-            getKlimaat(toDate("01-01-2016 14:00:00"), new BigDecimal("20.00")),
-            getKlimaat(toDate("01-01-2016 14:04:00"), new BigDecimal("20.00")),
-            getKlimaat(toDate("01-01-2016 14:08:00"), new BigDecimal("20.00"))
+            createKlimaat(LocalDate.of(2016, JULY, 1).atTime(14, 0, 0), new BigDecimal("20.00")),
+            createKlimaat(LocalDate.of(2016, JULY, 1).atTime(14, 4, 0), new BigDecimal("20.00")),
+            createKlimaat(LocalDate.of(2016, JULY, 1).atTime(14, 8, 0), new BigDecimal("20.00"))
         );
 
         assertThat(klimaatSensorValueTrendService.determineValueTrend(klimaats, Klimaat::getTemperatuur)).isEqualTo(Trend.STABLE);
     }
 
     @Test
-    public void whenNotEnoughValidSamplesThenTrendIsUndetermined() throws Exception {
+    public void whenNotEnoughValidSamplesThenTrendIsUndetermined() {
         List<Klimaat> klimaats = asList(
-                getKlimaat(toDate("01-01-2016 14:00:00"), null),
-                getKlimaat(toDate("01-01-2016 14:04:00"), null),
-                getKlimaat(toDate("01-01-2016 14:08:00"), null)
+                createKlimaat(LocalDate.of(2016, JULY, 1).atTime(14, 0, 0), null),
+                createKlimaat(LocalDate.of(2016, JULY, 1).atTime(14, 4, 0), null),
+                createKlimaat(LocalDate.of(2016, JULY, 1).atTime(14, 8, 0), null)
         );
 
         assertThat(klimaatSensorValueTrendService.determineValueTrend(klimaats, Klimaat::getTemperatuur)).isNull();
     }
 
     @Test
-    public void whenNotEnoughSamplesThenTrendIsUndetermined() throws Exception {
+    public void whenNotEnoughSamplesThenTrendIsUndetermined() {
         assertThat(klimaatSensorValueTrendService.determineValueTrend(emptyList(), Klimaat::getTemperatuur)).isNull();
     }
 
-    private Klimaat getKlimaat(Date datumtijd, BigDecimal temperatuur) throws ParseException {
+    private Klimaat createKlimaat(LocalDateTime datumtijd, BigDecimal temperatuur) {
         Klimaat klimaat = new Klimaat();
-        klimaat.setDatumtijd(DateTimeUtil.toLocalDateTime(datumtijd));
+        klimaat.setDatumtijd(datumtijd);
         klimaat.setTemperatuur(temperatuur);
         klimaat.setKlimaatSensor(klimaatSensor);
         return klimaat;
-    }
-
-    private Date toDate(String dateString) throws ParseException {
-        return DateUtils.parseDate(dateString, "dd-MM-yyyy HH:mm:ss");
     }
 }
