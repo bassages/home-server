e24f8d278e188ea6c322a1f6712abe106ce99b88
Rob Wiegman <rob.wiegman@luminis.eu>
2018-02-03 18:20:35 +0100

    Add Project Lombok

--- src/main/java/nl/homeserver/ErrorResponse.java
+++ src/main/java/nl/homeserver/ErrorResponse.java
@@ -1,24 +1,13 @@
 package nl.homeserver;
 
-import org.apache.commons.lang3.exception.ExceptionUtils;
-
 import lombok.Getter;
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class ErrorResponse {
 
-    @Getter @Setter
-    private String code;
-    @Getter @Setter
-    private String details;
-
-    public ErrorResponse(Throwable t) {
-        code = t.getClass().getName();
-        details = ExceptionUtils.getStackTrace(t);
-    }
-
-    public ErrorResponse(String code, String details) {
-        this.code = code;
-        this.details = details;
-    }
+    @Getter
+    private final String code;
+    @Getter
+    private final String details;
 }
