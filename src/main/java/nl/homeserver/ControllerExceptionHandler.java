81712ec200a05cea1ebf4c5999afcec6c6640218
Rob Wiegman <rob.wiegman@luminis.eu>
2018-02-03 19:28:28 +0100

    Add unitTests for ControllerExceptionHandler

--- src/main/java/nl/homeserver/ControllerExceptionHandler.java
+++ src/main/java/nl/homeserver/ControllerExceptionHandler.java
@@ -2,25 +2,25 @@ package nl.homeserver;
 
 import static org.apache.commons.lang3.exception.ExceptionUtils.getStackTrace;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
-    private static final String ERROR_CODE_UNIQUE_KEY_CONSTRAINT_VIOLATION = "UNIQUE_KEY_CONSTRAINT_VIOLATION";
-    private static final String ERROR_CODE_NOT_FOUND = "NOT_FOUND";
+    protected static final String ERROR_CODE_UNIQUE_KEY_CONSTRAINT_VIOLATION = "UNIQUE_KEY_CONSTRAINT_VIOLATION";
+    protected static final String ERROR_CODE_NOT_FOUND = "NOT_FOUND";
 
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<ErrorResponse> onDataIntegrityViolationException(DataIntegrityViolationException ex) {
-        return new ResponseEntity<>(new ErrorResponse(ERROR_CODE_UNIQUE_KEY_CONSTRAINT_VIOLATION, getStackTrace(ex)), BAD_REQUEST);
+    public ResponseEntity<ErrorResponse> createResponse(DataIntegrityViolationException dataIntegrityViolationException) {
+        return new ResponseEntity<>(new ErrorResponse(ERROR_CODE_UNIQUE_KEY_CONSTRAINT_VIOLATION, getStackTrace(dataIntegrityViolationException)), BAD_REQUEST);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorResponse> requestHandlingNoHandlerFound(ResourceNotFoundException resourceNotFoundException) {
-        return new ResponseEntity<>(new ErrorResponse(ERROR_CODE_NOT_FOUND, resourceNotFoundException.getMessage()), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponse> createResponse(ResourceNotFoundException resourceNotFoundException) {
+        return new ResponseEntity<>(new ErrorResponse(ERROR_CODE_NOT_FOUND, resourceNotFoundException.getMessage()), NOT_FOUND);
     }
 }
---
+++ src/test/java/nl/homeserver/ControllerExceptionHandlerTest.java
@@ -0,0 +1,42 @@
+package nl.homeserver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ControllerExceptionHandlerTest {
+
+    @Test
+    public void givenDataIntegrityViolationExceptionWhenCreateResponseThenErrorResponseCreated() {
+        ControllerExceptionHandler controllerExceptionHandler = new ControllerExceptionHandler();
+
+        String message = "FUBAR";
+        DataIntegrityViolationException exception = new DataIntegrityViolationException(message);
+
+        ResponseEntity<ErrorResponse> errorResponseResponseEntity = controllerExceptionHandler.createResponse(exception);
+
+        assertThat(errorResponseResponseEntity.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        assertThat(errorResponseResponseEntity.getBody().getCode()).isEqualTo(ControllerExceptionHandler.ERROR_CODE_UNIQUE_KEY_CONSTRAINT_VIOLATION);
+        assertThat(errorResponseResponseEntity.getBody().getDetails()).contains(message);
+    }
+
+    @Test
+    public void givenResourceNotFoundExceptionWhenCreateResponseThenErrorResponseCreated() {
+        ControllerExceptionHandler controllerExceptionHandler = new ControllerExceptionHandler();
+
+        String message = "FUBAR";
+        ResourceNotFoundException exception = new ResourceNotFoundException(message, null);
+
+        ResponseEntity<ErrorResponse> errorResponseResponseEntity = controllerExceptionHandler.createResponse(exception);
+
+        assertThat(errorResponseResponseEntity.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(errorResponseResponseEntity.getBody().getCode()).isEqualTo(ControllerExceptionHandler.ERROR_CODE_NOT_FOUND);
+        assertThat(errorResponseResponseEntity.getBody().getDetails()).contains(message);
+    }
+}
