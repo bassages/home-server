464564650d90f2051776a124f72bfc2be563c485
Rob Wiegman <rob.wiegman@luminis.eu>
2018-02-03 20:03:52 +0100

    Replace @Data by @Getter and @Setter

--- src/main/java/nl/homeserver/klimaat/RealtimeKlimaat.java
+++ src/main/java/nl/homeserver/klimaat/RealtimeKlimaat.java
@@ -3,14 +3,19 @@ package nl.homeserver.klimaat;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import nl.homeserver.Trend;
 
-@Data
 public class RealtimeKlimaat {
+    @Getter @Setter
     private LocalDateTime datumtijd;
+    @Getter @Setter
     private BigDecimal temperatuur;
+    @Getter @Setter
     private BigDecimal luchtvochtigheid;
+    @Getter @Setter
     private Trend temperatuurTrend;
+    @Getter @Setter
     private Trend luchtvochtigheidTrend;
 }
