67406b6d78a1b0addfe2de6dcf6a4f76ffe34463
Rob Wiegman <rob.wiegman@luminis.eu>
2018-02-03 20:09:05 +0100

    Replace @Data by @Getter and @Setter

--- src/main/java/nl/homeserver/energie/VerbruikKostenOverzicht.java
+++ src/main/java/nl/homeserver/energie/VerbruikKostenOverzicht.java
@@ -2,17 +2,32 @@ package nl.homeserver.energie;
 
 import java.math.BigDecimal;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
 public class VerbruikKostenOverzicht {
 
+    @Getter
+    @Setter
     private BigDecimal stroomVerbruikDal;
+
+    @Getter
+    @Setter
     private BigDecimal stroomKostenDal;
 
+    @Getter
+    @Setter
     private BigDecimal stroomVerbruikNormaal;
+
+    @Getter
+    @Setter
     private BigDecimal stroomKostenNormaal;
 
+    @Getter
+    @Setter
     private BigDecimal gasVerbruik;
+
+    @Getter
+    @Setter
     private BigDecimal gasKosten;
 }
