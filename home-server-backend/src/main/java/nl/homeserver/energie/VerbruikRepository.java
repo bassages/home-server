76051572c72356e5a0e8b3fab2b863d43660a2c2
Rob Wiegman <rob.wiegman@luminis.eu>
2018-09-12 20:36:05 +0200

    Add nullable annotations

--- home-server-backend/src/main/java/nl/homeserver/energie/VerbruikRepository.java
+++ home-server-backend/src/main/java/nl/homeserver/energie/VerbruikRepository.java
@@ -3,6 +3,7 @@ package nl.homeserver.energie;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import javax.annotation.Nullable;
 import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,12 +13,15 @@ import org.springframework.data.repository.query.Param;
 @Transactional
 public interface VerbruikRepository extends JpaRepository<Meterstand, Long> {
 
+    @Nullable
     @Query(value = "SELECT (MAX(stroom_tarief2) - MIN(stroom_tarief2)) FROM meterstand WHERE date_time >= :van AND date_time < :tot", nativeQuery = true)
     BigDecimal getStroomVerbruikNormaalTariefInPeriod(@Param("van") LocalDateTime van, @Param("tot") LocalDateTime tot);
 
+    @Nullable
     @Query(value = "SELECT (MAX(stroom_tarief1) - MIN(stroom_tarief1)) FROM meterstand WHERE date_time >= :van AND date_time < :tot", nativeQuery = true)
     BigDecimal getStroomVerbruikDalTariefInPeriod(@Param("van") LocalDateTime van, @Param("tot") LocalDateTime tot);
 
+    @Nullable
     @Query(value = "SELECT MAX(gas) - MIN(gas) FROM meterstand WHERE date_time >= :van AND date_time < :tot", nativeQuery = true)
     BigDecimal getGasVerbruikInPeriod(@Param("van") LocalDateTime van, @Param("tot") LocalDateTime tot);
 }
