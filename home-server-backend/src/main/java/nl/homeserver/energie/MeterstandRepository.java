92769147ed3dbe167aaf2441d2f6d649e524a6a2
Rob Wiegman <rob.wiegman@luminis.eu>
2018-09-13 18:37:40 +0200

    Add nullable annotations

--- home-server-backend/src/main/java/nl/homeserver/energie/MeterstandRepository.java
+++ home-server-backend/src/main/java/nl/homeserver/energie/MeterstandRepository.java
@@ -1,34 +1,42 @@
 package nl.homeserver.energie;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import javax.transaction.Transactional;
 import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+
+import javax.annotation.Nullable;
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @Transactional
 public interface MeterstandRepository extends JpaRepository<Meterstand, Long> {
 
     // Please note that BETWEEN in HQL is INCLUSIVE!
 
+    @Nullable
     @Query(value = "SELECT m FROM Meterstand m WHERE m.dateTime = (SELECT MAX(mostrecent.dateTime) FROM Meterstand mostrecent)")
     Meterstand getMostRecent();
 
+    @Nullable
     @Query(value = "SELECT m FROM Meterstand m WHERE m.dateTime = (SELECT MIN(oldest.dateTime) FROM Meterstand oldest)")
     Meterstand getOldest();
 
+    @Nullable
     @Query(value = "SELECT m FROM Meterstand m WHERE m.dateTime = (SELECT MAX(dateTime) from Meterstand m where m.dateTime BETWEEN :van AND :totEnMet)")
     Meterstand getMostRecentInPeriod(@Param("van") LocalDateTime start, @Param("totEnMet") LocalDateTime end);
 
+    @Nullable
     @Query(value = "SELECT m FROM Meterstand m WHERE m.dateTime = (SELECT MIN(dateTime) from Meterstand m where m.dateTime BETWEEN :van AND :totEnMet)")
     Meterstand getOldestInPeriod(@Param("van") LocalDateTime start, @Param("totEnMet") LocalDateTime end);
 
+    @Nullable
     List<Meterstand> findByDateTimeBetween(LocalDateTime start, LocalDateTime end);
 
+    @Nullable
     @Query(value = "SELECT date FROM (" +
                    "  SELECT PARSEDATETIME(FORMATDATETIME(date_time, 'dd-MM-yyyy'), 'dd-MM-yyyy') AS date, " +
                    "         COUNT(id) AS nr_of_records " +
