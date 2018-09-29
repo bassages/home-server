1ebef24269ca8f6cfc45999488b9f92c2bec2eb1
Rob Wiegman <rob.wiegman@luminis.eu>
2018-09-29 15:41:45 +0200

    Add homeserver-db-maintainance project

---
+++ home-server-db-maintainance/src/main/java/nl/homeserver/HomeServerDbMaintainanceApplication.java
@@ -0,0 +1,12 @@
+package nl.homeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServerDbMaintainanceApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(HomeServerDbMaintainanceApplication.class, args);
+    }
+}
