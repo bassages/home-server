ad085d0495cf592828e724f0cca2f3e04e102ad4
Rob Wiegman <rob.wiegman@luminis.eu>
2018-11-03 19:37:23 +0100

    Update to java 11 / Spring Boot 2.1.0

--- home-server-backend/src/test/java/nl/homeserver/LoggingRule.java
+++ home-server-backend/src/test/java/nl/homeserver/LoggingRule.java
@@ -1,60 +1,31 @@
 package nl.homeserver;
 
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.mock;
-import static org.slf4j.LoggerFactory.getLogger;
-
-import org.junit.rules.ExternalResource;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 import org.mockito.ArgumentCaptor;
 
 import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.spi.LoggingEvent;
-import ch.qos.logback.core.Appender;
 
 public class LoggingRule implements TestRule {
 
-    private final Logger logger;
-    private final Appender mockAppender;
-
-    private ArgumentCaptor<LoggingEvent> loggingEventCaptor;
+    private final LoggerEventCaptor loggerEventCaptor;
 
     public LoggingRule(final Class<?> loggerClass) {
-        this.logger = (Logger) getLogger(loggerClass);
-        mockAppender = mock(Appender.class);
-    }
-
-    private final SetUpAndTearDown setUpAndTearDown = new SetUpAndTearDown();
-
-    @SuppressWarnings("unchecked")
-    private class SetUpAndTearDown extends ExternalResource {
-        @Override
-        protected void before() {
-            loggingEventCaptor = ArgumentCaptor.forClass(LoggingEvent.class);
-            doAnswer(invocation -> null).when(mockAppender).doAppend(loggingEventCaptor.capture());
-            logger.addAppender(mockAppender);
-        }
-
-        @Override
-        protected void after() {
-            logger.detachAppender(mockAppender);
-        }
+        loggerEventCaptor = new LoggerEventCaptor(loggerClass);
     }
 
     @Override
     public Statement apply(final Statement base, final Description description) {
-        return setUpAndTearDown.apply(base, description);
+        return loggerEventCaptor.apply(base, description);
     }
 
     public void setLevel(final Level level) {
-        logger.setLevel(level);
+        loggerEventCaptor.setLevel(level);
     }
 
     public ArgumentCaptor<LoggingEvent> getLoggedEventCaptor() {
-        return loggingEventCaptor;
+        return loggerEventCaptor.getLoggedEventCaptor();
     }
-
 }
