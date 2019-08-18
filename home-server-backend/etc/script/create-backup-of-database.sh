e2a26fe55ea021884fa5cf564c2e074904b64463
Rob Wiegman <rob.wiegman@luminis.eu>
2019-08-18 11:11:04 +0200

    Updated dependencies

--- home-server-backend/etc/script/create-backup-of-database.sh
+++ home-server-backend/etc/script/create-backup-of-database.sh
@@ -7,7 +7,8 @@ sleep 10
 
 echo "Copy database"
 today=`date +%Y%m%d%H%M%S`
-cp -R database home-server.database.backup.${today}
+#cp -R database home-server.database.backup.${today}
+zip -r home-server.database.backup.${today}.zip database/
 
 echo "Start service"
 sudo service home-server start
