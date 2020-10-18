286e21135ed07fef556f4c8324889314a8869816
Rob Wiegman <bassages@gmail.com>
2020-10-18 15:47:44 +0200

    Use Lombok for constructor

--- src/main/java/nl/homeserver/housekeeping/HousekeepingSchedule.java
+++ src/main/java/nl/homeserver/housekeeping/HousekeepingSchedule.java
@@ -1,5 +1,9 @@
 package nl.homeserver.housekeeping;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class HousekeepingSchedule {
     private static final String FIVE_MINUTES_PAST_MIDNIGHT = "0 5 0 * * *";
     private static final String ONE_AM = "0 0 1 * * *";
@@ -8,6 +12,4 @@ public final class HousekeepingSchedule {
     public static final String WARMUP_CACHE = FIVE_MINUTES_PAST_MIDNIGHT;
     public static final String OPGENOMEN_VERMOGEN_CLEANUP = ONE_AM;
     public static final String METERSTAND_CLEANUP = TWO_AM;
-
-    private HousekeepingSchedule() { }
 }
