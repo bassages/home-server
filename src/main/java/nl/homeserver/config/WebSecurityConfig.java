fb325c4654b6614cb205565791037a5044bed5d1
Rob <bassages@gmail.com>
2023-01-02 19:56:47 +0100

    Remove unused imports, remove unneeded @SuppressWarnings

--- src/main/java/nl/homeserver/config/WebSecurityConfig.java
+++ src/main/java/nl/homeserver/config/WebSecurityConfig.java
@@ -1,7 +1,5 @@
 package nl.homeserver.config;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
@@ -9,12 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.RememberMeServices;
 
 import static org.springframework.http.HttpStatus.RESET_CONTENT;
 
@@ -30,7 +23,6 @@ public class WebSecurityConfig {
         this.unauthenticatedRequestHandler = unauthenticatedRequestHandler;
     }
 
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Bean
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
         http
