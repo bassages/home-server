e2668bd9120076898210185e98dff156d0aecbd3
Rob <bassages@gmail.com>
2026-06-22 18:10:28 +0200

    No need to use Date

--- src/main/java/nl/homeserver/DateTimeUtil.java
+++ src/main/java/nl/homeserver/DateTimeUtil.java
@@ -5,12 +5,11 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.Date;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class DateTimeUtil {
 
     public static long toMillisSinceEpoch(final LocalDateTime localDateTime) {
-        return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant()).getTime();
+        return localDateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
     }
 }
